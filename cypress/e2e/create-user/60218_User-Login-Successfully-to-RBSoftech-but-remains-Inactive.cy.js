502cc6959da8a188db82bfd408f90ddea885d34a
Rocky llamasares <rockyllamasares8@gmail.com>
2026-02-03 09:20:28 +0800

    Add new scenarios for login users

---
+++ cypress/e2e/create-user/60218_User-Login-Successfully-to-RBSoftech-but-remains-Inactive.cy.js
@@ -0,0 +1,5 @@
+describe('Scenario : User Login successfully to RBSoftech but remains Inactive',() => {
+    it('Inactive user account (no mouse movement or click)', function () {
+        cy.InActiveUserAccount();
+    });
+});
