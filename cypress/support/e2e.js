0c506bb03ebb188e69567be0c110660951599f1d
Rocky llamasares <rockyllamasares8@gmail.com>
2026-02-05 11:06:50 +0800

    Update e2e.js import a new command for accounting date

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -64,6 +64,7 @@ import '@support/create-user/CreateUserCommand';
  * Accounting
  */
 import '@support/commands/Accounting/Journals/JournalPostingCommand';
+import '@support/commands/Accounting/Acctng-Date/AcctngDateCommand';
 
 /**
  * Lending
